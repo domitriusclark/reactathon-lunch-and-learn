51954cc076b1f08a95eb6753bf5f6f01881bb5e2
Domitrius Clark <domitriusaclark@gmail.com>
2023-05-03 16:44:40 -0700

    Removed dead import and added .env to gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <h1 className="text-2xl text-white">Home Page</h1>

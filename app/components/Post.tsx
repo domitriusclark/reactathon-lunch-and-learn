41d30c9c636b9c64c9ca951e9cfaf7b3702f4db3
Domitrius Clark <domitriusaclark@gmail.com>
2023-05-25 01:17:12 -0400

    Adding UI

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,31 +1,31 @@
-// This component will not be useable until you generate your xata client. Uncomment once you've done that!
+import { PostsRecord } from "@/lib/xata.codegen";
 
-// import this from wherever you xata client is generated
-// import { PostsRecord } from "@/lib/xata.codegen";
+import UpvoteButton from "./UpvoteButton";
 
-// type Props = {
-//   post: PostsRecord;
-//   key: string;
-// };
+type Props = {
+  post: PostsRecord;
+  key: string;
+};
 
-// export default function Post({ post, key }: Props) {
-//   return (
-//     <article
-//       key={key}
-//       className="w-full p-4 text-white border-2 rounded-lg border-slate-300 "
-//     >
-//       <h1 className="text-xl">{post.title}</h1>
-//       <p className="text-md">{post.text}</p>
-//       <div className="flex flex-wrap gap-2 mt-3">
-//         {post.tags?.map((tag) => (
-//           <span
-//             key={tag}
-//             className="px-2 py-1 text-sm text-white rounded-lg bg-slate-700"
-//           >
-//             {tag}
-//           </span>
-//         ))}
-//       </div>
-//     </article>
-//   );
-// }
+export default function Post({ post, key }: Props) {
+  return (
+    <article
+      key={key}
+      className="w-full p-4 text-white border-2 rounded-lg border-slate-300 "
+    >
+      <h1 className="text-xl">{post.title}</h1>
+      <p className="text-md">{post.text}</p>
+      <div className="flex flex-wrap gap-2 mt-3">
+        {post.tags?.map((tag) => (
+          <span
+            key={tag}
+            className="px-2 py-1 text-sm text-white rounded-lg bg-slate-700"
+          >
+            {tag}
+          </span>
+        ))}
+        <UpvoteButton />
+      </div>
+    </article>
+  );
+}
